23325bf72fd909b471372d3e74c468fd20cb7ad1
afifeka <37196374+afifeka@users.noreply.github.com>
2018-06-19 22:09:53 +0700

    Create help.js

---
+++ cmds/help.js
@@ -0,0 +1,55 @@
+const Discord = require("discord.js");
+const pkg = require("../package.json");
+
+exports.run = async (bot, message, args) => {
+
+    if (!args[0]){
+    let pages = [`**Member**\n> \`avatar\` - Tampilkan foto profil  seseorang
+> \`ping\` -  Melihat kecepatan internet kamu
+> \`report\` - Report orang mbeling
+> \`invite\` - Invite bot **${bot.username}**`, `**Moderation**\n>\`ban\` - Banned orang melanggar peraturan
+> \`kick\` - Kick orang yang nakal
+> \`warn\` - Warned orang buka bokep`];
+    let page = 1;
+    
+    const embed = new Discord.RichEmbed()
+    .setColor("#f6f940")
+    .setFooter(`Page ${page} of ${pages.length} • © ${bot.username} | Alpha ${pkg.version}`)
+    .setDescription(pages[page-1])
+    
+    message.channel.send(embed).then(msg => {
+    
+    msg.react(`◀`).then( r => {
+    msg.react(`▶`)
+    
+    const backwardsFilter = (reaction, user) => reaction.emoji.name === `◀` && user.id === message.author.id;
+    const forwardsFilter = (reaction, user) => reaction.emoji.name === `▶` && user.id === message.author.id;
+
+    const backwards = msg.createReactionCollector(backwardsFilter, { time:60000 });
+    const forwards = msg.createReactionCollector(forwardsFilter, { time:60000 });
+
+    backwards.on('collect', r => {
+        if (page === 1) return;
+        page--;
+        embed.setDescription(pages[page-1]);
+        embed.setFooter(`Page ${page} of ${pages.length} • © ${pkg.name} | Alpha ${pkg.version}`)
+        msg.edit(embed)
+    })
+    
+    forwards.on('collect', r => {
+    if (page === pages.length) return;
+        page++;
+        embed.setDescription(pages[page-1]);
+        embed.setFooter(`Page ${page} of ${pages.length} • © ${pkg.name} | Alpha ${pkg.version}`)
+        msg.edit(embed)
+        });
+    });
+});
+    }
+}
+
+exports.help = {
+    name: "help",
+    description: 'Menampilkan semua perintah',
+    usage: '~help <perintah>'
+}
