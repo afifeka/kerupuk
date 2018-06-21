5ee80931d98680a70ae770465ab1ec4b13a5e077
afifeka <37196374+afifeka@users.noreply.github.com>
2018-06-21 09:13:07 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: false});
+const cfg = require("./botconfig.json");
+const fs = require("fs");
+const colors = require('colors');
+const moment = require('moment');
+const snekfetch = require("snekfetch");
+const prefixes = require("./prefixes.json")
+const db = require('quick.db');
+const figlet = require('figlet');
+
+bot.on("message", async message => {
+    if(message.author.bot) return;
+	
+    let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+    if (!prefixes[message.guild.id]) {
+        prefixes[message.guild.id] = {
+            prefixes: cfg.prefix
+        };
+    }
+    let prefix = prefixes[message.guild.id].prefixes;
+    let msg = message.content.toLowerCase();
+    let sender = message.author;
+    let args = message.content.slice(prefix.length).trim().split(" ");
+    let cmd = args.shift().toLowerCase();
+
+    if (message.content === `<@${bot.user.id}>`) {
+        message.channel.send(`Hello <@${message.author.id}>, Cloud With Prefix \`${prefix}\``);
+        message.react('🆗');
+    }
+    
+
+    if (sender.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    try {
+        let commandFile = require(`./cmds/${cmd}.js`);
+        commandFile.run(bot, message, args);
+    } catch(e) {
+        console.log(e.message);
+    } finally {
+        console.log(`${message.author.username} | ${cmd} | ${message.guild.name}`);
+    }
+});
+
+bot.on("guildMemberAdd", member => {
+	let autorole = JSON.parse(fs.readFileSync("./autorole.json", "utf8"));
+	if (!autorole[member.guild.id]) {
+		autorole[member.guild.id] = {
+			autorole: cfg.autorole
+		};
+	}
+	var role = autorole[member.guild.id].role;
+	if (!role)
+	member.addRole(role);
+});
+
+bot.on("ready", async () => {
+    console.log(`${bot.user.tag} is ready!`);
+    bot.users.get("401327121580032000").send(`${bot.user.tag} Has Online, Success Reloaded All File's`);
+   
+});
+
+bot.login(process.env.BOT_TOKEN);
