2bcd98d0b7d691b21ca4c780c613762639c137ba
afifeka <37196374+afifeka@users.noreply.github.com>
2018-06-21 22:27:32 +0700

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -7,14 +7,15 @@ exports.run = async (bot, message, args) => {
     let pages = [`**Member**\n> \`avatar\` - Tampilkan foto profil  seseorang
 > \`ping\` -  Melihat kecepatan internet kamu
 > \`report\` - Report orang mbeling
-> \`invite\` - Invite bot **${bot.username}**`, `**Moderation**\n> \`ban\` - Banned orang melanggar peraturan
+> \`invite\` - Invite bot **<@458654300575498270>**`, `**Moderation**\n> \`ban\` - Banned orang melanggar peraturan
 > \`kick\` - Kick orang yang nakal
-> \`warn\` - Warned orang buka bokep`];
+> \`warn\` - Warned orang buka bokep`, `Core\n> \`autorole\` - Menetapkan untuk auto Role\n
+> \`setprefix\` - Menetapkan prefix dalam server`];
     let page = 1;
     
     const embed = new Discord.RichEmbed()
     .setColor("#f6f940")
-    .setFooter(`Page ${page} of ${pages.length} • © ${bot.username} | Alpha ${pkg.version}`)
+    .setFooter(`Page ${page} of ${pages.length} • © ${bot.user.username} | Alpha ${pkg.version}`)
     .setDescription(pages[page-1])
     
     message.channel.send(embed).then(msg => {
@@ -44,47 +45,7 @@ exports.run = async (bot, message, args) => {
         msg.edit(embed)
         });
     });
-});
-    }
-    if (args[0] === 'admin') {
-    let pages2 = [`**Page: 1**\n\`addrole\` - berikan role kepada seseorang.\n\`ban\` - banned seseorang dari guild.\n\`kick\` - kick seseorang dari guild.\n\`mute\` - mute seseorang.`,`
-     **Page: 2**\n\`remrole\` - hapus role dari seseorang.\n\`unban\` - Unban seseorang.\n\`warn\` - Warn seseorang.\n\`warnings\` -  Periksa jumlah warning`];
-    let page2 = 1;
-    
-    const embed2 = new Discord.RichEmbed()
-    .setColor("RANDOM")
-    .setFooter(`Page ${page2} of ${pages2.length} • © ${pkg.name} | Alpha ${pkg.version}`)
-    .setDescription(pages2[page2-1])
-    
-    message.channel.send(embed2).then(msg => {
-    
-    msg.react(`◀`).then( r => {
-    msg.react(`▶`)
-    
-    const backwardsFilter = (reaction, user) => reaction.emoji.name === `◀` && user.id === message.author.id;
-    const forwardsFilter = (reaction, user) => reaction.emoji.name === `▶` && user.id === message.author.id;
-
-    const backwards = msg.createReactionCollector(backwardsFilter, { time:60000 });
-    const forwards = msg.createReactionCollector(forwardsFilter, { time:60000 });
-
-    backwards.on('collect', r => {
-        if (page2 === 1) return;
-            page2--;
-            embed2.setDescription(pages2[page2-1]);
-            embed2.setFooter(`Page ${page2} of ${pages2.length} • © ${pkg.name} | Alpha ${pkg.version}`)
-            msg.edit(embed2)
-    });
-    
-    forwards.on('collect', r => {
-        if (page2 === pages2.length) return;
-            page2++;
-            embed2.setDescription(pages2[page2-1]);
-            embed2.setFooter(`Page ${page2} of ${pages2.length} • © ${pkg.name} | Alpha ${pkg.version}`)
-            msg.edit(embed2)
-            });
-        });
-    });
-    }
+ });
 }
 
 exports.help = {
