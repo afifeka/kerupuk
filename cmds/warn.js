4e1173f096d5d29b9f74e7eb0da3150a667ee2f9
afifeka <37196374+afifeka@users.noreply.github.com>
2018-06-21 17:21:36 +0700

    Update warn.js

--- cmds/warn.js
+++ cmds/warn.js
@@ -1,4 +1,6 @@
-exports.help = (bot, message, args) => {
+const Discord = require("discord.js");
+
+exports.run = (bot, message, args) => {
 		let wUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
         if(!wUser) return message.channel.send("User tidak ditemukan!");
         let wReason = args.join(" ").slice(22);
