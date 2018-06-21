76ec9ffe230a5a3e28256180e6def7916a236074
afifeka <37196374+afifeka@users.noreply.github.com>
2018-06-21 17:19:34 +0700

    Update ban.js

--- cmds/ban.js
+++ cmds/ban.js
@@ -1,4 +1,6 @@
-exports.help = (bot, message, args) => {
+const Discord = require("discord.js");
+
+exports.run = (bot, message, args) => {
 	    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!bUser) return message.channel.send("User tidak ditemukan");
     let bReason = args.join(" ").slice(22);
