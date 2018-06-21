3407f800fc44d210e1997ebbb30a3e6e9a540e7f
afifeka <37196374+afifeka@users.noreply.github.com>
2018-06-21 09:48:36 +0700

    Create eval.js

---
+++ cmds/eval.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+exports.run = (bot, message, args) => {
+       if (message.author.id !== '401327121580032000') return;
+    try {
+        let codein = args.join(" ");
+        let code = eval(codein);
+
+        if (typeof code !== 'string')
+            code = require('util').inspect(code, { depth: 0 });
+        let embed = new Discord.RichEmbed()
+        .setAuthor('Evaluate to Kerupuk')
+        .setColor("#c8f442")
+        .addField('📥 Input', `\`\`\`js\n${codein}\`\`\``)
+        .addField('📤 Output', `\`\`\`js\n${code}\n\`\`\``)
+        message.channel.send(embed)
+    } catch(e) {
+        message.channel.send(`\`\`\`js\n${e}\n\`\`\``);
+    }
+}
+
+exports.help = {
+  name: "eval"
+}
