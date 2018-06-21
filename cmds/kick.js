04f8f879902d71d52d054be0763ed8dbdd6ae3a8
afifeka <37196374+afifeka@users.noreply.github.com>
2018-06-21 15:07:35 +0700

    Update kick.js

--- cmds/kick.js
+++ cmds/kick.js
@@ -1,25 +1,25 @@
 exports.help = (bot, message, args) => {
 	    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!kUser) return message.channel.send("Can't find user!");
+    if(!kUser) return message.channel.send("User tidak ditemukan");
     let kReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
-    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Anda tidak ada izin untuk menggunakan command ini");								 ");
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Tidak bisa kick user tersebut ");
 
     let kickEmbed = new Discord.RichEmbed()
     .setDescription("Kick")
     .setColor("#e56b00")
-    .addField("Kicked User", `**${kUser}**`)
-    .addField("Kicked By", `**<@${message.author.id}>**`)
+    .addField("User", `**${kUser}**`)
+    .addField("Moderator", `**<@${message.author.id}>**`)
     .addField("Reason", `${kReason}`);
 
     let kickChannel = message.guild.channels.find(`name`, "mod-logs");
-    if(!kickChannel) return message.channel.send("Can't find **mod-logs** channel.");
+    if(!kickChannel) return message.channel.send("Channel **mod-logs** tidak ditemukan.");
 
     message.guild.member(kUser).kick(kReason);
     kickChannel.send(kickEmbed);
 	  
     message.delete().catch(O_o=>{});
-    message.channel.send("Success kicked the player");
+    message.channel.send("Sukses kick user tersebut");
 
     return;
 }
