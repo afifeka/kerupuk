9354ad1d7f82a311eb7139b6bcdb8088f8ab1948
afifeka <37196374+afifeka@users.noreply.github.com>
2018-06-22 09:13:37 +0700

    Create setprefix.js

---
+++ cmds/setprefix.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
+exports.run = async (bot, message, args, color) => {
+
+    if (!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send("Anda tidak ada izin untuk menggunakan command ini");
+    
+    let prefixes = JSON.parse(fs.readFileSync('./prefixes.json', 'utf8'));
+    prefixes[message.guild.id] = {
+        prefixes: args[0]
+    };
+
+    fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
+        if (err) console.log(err)
+    });
+
+    return message.channel.send(`Prefix baru diserver ini adalah \`${args[0]}\`! `);
+}
+
+exports.help = {
+    name: "prefix",
+    description: "Change your server prefix",
+    usage: "suzu prefix <new prefix>"
+}
