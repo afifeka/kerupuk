907287b8bffbded6867cfd62764a7c7582fdb2d0
afifeka <37196374+afifeka@users.noreply.github.com>
2018-07-06 22:36:32 +0700

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -9,7 +9,8 @@ exports.run = async (bot, message, args) => {
 > \`report\` - Report orang mbeling
 > \`invite\` - Invite bot **<@458654300575498270>**`, `**Moderation**\n> \`ban\` - Banned orang melanggar peraturan
 > \`kick\` - Kick orang yang nakal
-> \`warn\` - Warned orang buka bokep`, `Core\n> \`autorole\` - Menetapkan untuk auto Role\n> \`setprefix\` - Menetapkan prefix dalam server`];
+> \`warn\` - Warned orang buka bokep`, `Core\n> \`autorole\` - Menetapkan untuk auto Role\n> \`setprefix\` - Menetapkan prefix dalam server`, `Nfsw\n
+> \`hentai\` - Lihat Anime Gk Pake Baju\n> hentaigif - Foto gif buat orang bego`];
     let page = 1;
     
     const embed = new Discord.RichEmbed()
